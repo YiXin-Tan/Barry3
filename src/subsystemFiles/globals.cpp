6d12fb4ed9b9cb79572a8bffe3a89adad267f720
YiXin-Tan <yixin360@gmail.com>
2021-07-29 20:36:40 +1000

    Updated motor spin direction

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -1,10 +1,10 @@
 #include "main.h"
 
 //MOTOR OBJECTS DEFINITION
-pros::Motor driveLeftFront(MTR_LEFT_FRONT_PORT, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor driveLeftBack(MTR_LEFT_BACK_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor driveRightFront(MTR_RIGHT_FRONT_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor driveRightBack(MTR_RIGHT_BACK_PORT, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveLeftFront(MTR_LEFT_FRONT_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveLeftBack(MTR_LEFT_BACK_PORT, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRightFront(MTR_RIGHT_FRONT_PORT, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRightBack(MTR_RIGHT_BACK_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
 //pros::Motor claw(MTR_CLAW_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor lift(MTR_LIFT_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor mini_lift(MTR_MINI_LIFT_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
